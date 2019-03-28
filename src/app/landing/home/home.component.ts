8af5b9fd1b8649b9e11923fae4a777b47594bda1
Gaurav <gaurav.kumar@venturepact.com>
2019-03-28 13:53:36 +0530

    mroe

--- src/app/landing/home/home.component.ts
+++ src/app/landing/home/home.component.ts
@@ -12,11 +12,40 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   selectedYear:  any;
+  joinUs: any = 'step1'
 
   ngOnInit() {
   }
 
   registerModal(){
     console.log("working");
+  }
+
+  createAccount(){
+    this.joinUs = 'step2';
+    console.log("profile created");
+  }
+
+  saveBasicInformation(){
+    this.joinUs = 'step3';
+    console.log("Basic information saved");
+  }
+
+  saveQualificationDetails(){
+    this.joinUs = 'step4';
+    console.log("Qualification Details saved");
+  }
+
+  dietHightCheck(){
+    this.joinUs = 'step5';
+    console.log("diet height");
+  }
+
+  almostDoneDetail(){
+    this.joinUs = 'step1';
+  }
+
+  authenticate(){
+    this.joinUs = 'step1';
   }
 }
